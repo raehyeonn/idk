cb0ba5eef0076f14d9d8499d0484aad593db59a0
Junggyun Park <92567159+terranking1@users.noreply.github.com>
2024-07-29 16:08:59 +0900

    feat(questionReport): 처리일자 필드 추가 (#15)

--- src/main/java/team/onepoom/idk/domain/questionReport/QuestionReport.java
+++ src/main/java/team/onepoom/idk/domain/questionReport/QuestionReport.java
@@ -7,12 +7,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.ZonedDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import team.onepoom.idk.domain.BaseEntity;
-import team.onepoom.idk.domain.Provider;
 import team.onepoom.idk.domain.question.Question;
 import team.onepoom.idk.domain.reportReason.ReportReason;
 import team.onepoom.idk.domain.user.User;
@@ -38,6 +38,8 @@ public class QuestionReport extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "question_id")
     private Question question;
+
+    private ZonedDateTime completedAt;
 
     //생성 메서드
     @Builder
