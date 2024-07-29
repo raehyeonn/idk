4d4afcd76f37872bfe515eed468dc466d2f28386
raehyeonn <zhhyeon32@naver.com>
2024-07-29 15:46:26 +0900

    feat(notice): 공지사항 엔티티 설계

---
+++ src/main/java/team/onepoom/idk/domain/notice/Notice.java
@@ -0,0 +1,44 @@
+package team.onepoom.idk.domain.notice;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.onepoom.idk.domain.BaseEntity;
+import team.onepoom.idk.domain.user.User;
+
+@Entity
+@Table(name = "notices")
+@NoArgsConstructor
+@Getter
+public class Notice extends BaseEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "writer_id")
+    private User writer;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 1000)
+    private String content;
+
+    @Column(nullable = false)
+    private int views;
+
+    public Notice(User writer, String title, String content) {
+        this.writer = writer;
+        this.title = title;
+        this.content = content;
+        this.views = 0;
+    }
+}
