4837937cbc1370ffd72c47a5570b269517b7b754
hyezuu <duudu0704@gmail.com>
2024-07-30 10:00:31 +0900

    feat(answer): AnswerRepository 추가

--- src/main/java/team/onepoom/idk/repository/AnswerRepository.java
+++ src/main/java/team/onepoom/idk/repository/AnswerRepository.java
@@ -1,5 +1,11 @@
 package team.onepoom.idk.repository;
 
-public interface AnswerRepository {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import team.onepoom.idk.domain.answer.Answer;
 
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
+    Page<Answer> findByWriter_Id(long id, Pageable pageable);
 }
