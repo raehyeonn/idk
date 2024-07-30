6388be1101c9c8073f433a09e68542c2ea7337e0
hyezuu <duudu0704@gmail.com>
2024-07-30 13:54:57 +0900

    feat(common): ErrorResponse(예외 응답 클래스) , ConflictException 추가

---
+++ src/main/java/team/onepoom/idk/common/exception/ConflictException.java
@@ -0,0 +1,8 @@
+package team.onepoom.idk.common.exception;
+
+public class ConflictException extends RuntimeException {
+
+    public ConflictException(String message) {
+        super(message);
+    }
+}
