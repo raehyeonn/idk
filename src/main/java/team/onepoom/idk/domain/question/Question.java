fd649ff36f856cf748f68206bb03755b1c2e4c4e
junggyun <onlyplsson@gmail.com>
2024-10-11 17:00:39 +0900

    test(cicd): 서버 ip 변경 후 cicd 테스트

--- src/main/java/team/onepoom/idk/domain/question/Question.java
+++ src/main/java/team/onepoom/idk/domain/question/Question.java
@@ -1,6 +1,5 @@
 package team.onepoom.idk.domain.question;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
